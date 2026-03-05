65f5994788b924c2f2907c0384db0058ea773ed6
ThatOneBloke <x1648811@gmail.com>
2026-03-05 17:59:47 +0000

    Add files via upload

---
+++ TextToSpeechV2.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import Spinbox
+import os
+from gtts import gTTS
+window = Tk()
+window.geometry("600x600")
+window.title("Text To speech")
+window.config(bg = "red")
+
+def TextToSpeech():
+    language = LanguageSpinbox.get()
+    myObject = gTTS(text = TextEntry.get(), lang = language, slow = False)
+    myObject.save("MySpeech.wav")
+    os.system("MySpeech.wav")
+
+frame1 = Frame(window, bg = "dark blue", height = "200")
+frame2 = Frame(window, bg = "blue", height = "400")
+Heading = Label(frame1, text = "text to speech", bg = "dark blue", fg = "light blue", font = ("Ariel", 30))
+EnterLabel = Label(frame1, text = "enter text here: ", bg = "dark blue", fg = "light blue", font = ("Ariel", 15))
+TextEntry = Entry(frame1, width = 30, bg = "light blue", fg = "blue")
+LanguageSpinbox = Spinbox(frame2, values = ("en", "fr", "es", "pl"))
+SubmitButton = Button(frame2, text = "submit", bg = "dark blue", fg = "light blue", command = TextToSpeech)
+
+frame1.pack(fill = X)
+frame2.pack(fill = X)
+Heading.place(x = 180, y = 75)
+EnterLabel.place(x = 150, y = 120)
+TextEntry.place(x = 300, y = 128)
+LanguageSpinbox.place(x = 300, y = 75)
+SubmitButton.place(x = 300, y = 200)
+
+
+window.mainloop()
